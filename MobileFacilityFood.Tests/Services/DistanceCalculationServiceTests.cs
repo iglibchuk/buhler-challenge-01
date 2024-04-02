1e04367c94b5f1cc670d05519f3afa980e96f22d
Ilya <inglibchuk@gmail.com>
2024-04-02 15:01:16 +0200

    use correct calculation instead of approximately

---
+++ MobileFacilityFood.Tests/Services/DistanceCalculationServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using MobileFacilityFood.Services;
+
+namespace MobileFacilityFood.Tests.Services;
+
+[TestClass]
+public class DistanceCalculationServiceTests
+{
+    [TestMethod]
+    public void Calculate_WhenCorrectData_ThenDistanceCalculatedApproximately()
+    {
+        var target = new DistanceCalculationService();
+
+        // taken from google
+        var result = target.Calculate(37.76396793169976, -122.51037344966151, 37.766153676572756, -122.46111937450482);
+
+        // distance in meters
+        Assert.IsTrue(result < 4500);
+        Assert.IsTrue(result > 4300);
+    }
+
+    [TestMethod]
+    public void Calculate_WhenCorrectData_ThenDistanceCalculatedApproximately2()
+    {
+        var target = new DistanceCalculationService();
+
+        // taken from google
+        var result = target.Calculate(37.76413856750102, -122.49651267682744, 37.75528592429814, -122.49599679946205);
+
+        // distance in meters
+        Assert.IsTrue(result < 1100);
+        Assert.IsTrue(result > 900);
+    }
+}
--- MobileFacilityFood/Services/DistanceCalculationService.cs
+++ MobileFacilityFood/Services/DistanceCalculationService.cs
@@ -2,13 +2,24 @@ namespace MobileFacilityFood.Services;
 
 public class DistanceCalculationService : IDistanceCalculationService
 {
+    const double R = 6371000; // Radius of the earth in meters
     public double Calculate(double point1Latitude, double point1Longitude, double point2Latitude, double point2Longitude)
     {
-        // the area is limited by San Francisco
-        // for quick calculation and clean code the distance could be calculated approximately 
-        // we count latitude and longitude as X and Y of a Cartesian coordinate system
-        var deltaLatitude = point1Latitude - point2Latitude;
-        var deltaLongitude = point1Longitude - point2Longitude;
-        return Math.Sqrt(deltaLatitude * deltaLatitude + deltaLongitude * deltaLongitude);
+        var dLat = DegreeTo2Radian(point2Latitude - point1Latitude);  // deg2rad below
+        var dLon = DegreeTo2Radian(point2Longitude - point1Longitude);
+
+        var a =
+                Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(DegreeTo2Radian(point1Latitude)) * Math.Cos(DegreeTo2Radian(point2Latitude)) *
+                Math.Sin(dLon / 2) * Math.Sin(dLon / 2)
+            ;
+        var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        var d = R * c; // Distance in km
+        return d;
+    }
+
+    private double DegreeTo2Radian(double deg)
+    {
+        return deg * (Math.PI / 180);
     }
 }
